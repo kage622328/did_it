1c501de53794d76c046fb54567f65f37849c3268
kage622328 <petit23_ma@yahoo.co.jp>
2024-07-23 14:00:57 +0900

    ジェネレーターの設定

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,12 @@ module DidIt
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    # 追記
+    config.generators do |g|
+      g.helper false             # helper ファイルを作成しない
+      g.test_framework false     # test ファイルを作成しない
+      g.skip_routes true         # ルーティングの記述を作成しない
+    end
+    # ここまで追記
   end
 end
