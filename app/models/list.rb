e0e271fff451788d99cf871dba74eb98ae7fd0bf
kage622328 <petit23_ma@yahoo.co.jp>
2024-07-29 16:43:57 +0900

    [Listモデル]修正

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,8 @@
 class List < ApplicationRecord
   belongs_to :child
 
-  validates :name, presence: true, length: { maximum: 10 }, 
+  validates :name, presence: true, length: { maximum: 10 }
+  validates :name, uniqueness: { scope: :child_id }
   
   enum status: { incomplete: 0, completed: 1 } 
 end
