953e15e8e96d63ee4ddea0b701c7ba4f23e10ffa
kage622328 <petit23_ma@yahoo.co.jp>
2024-08-01 00:01:51 +0900

    [ユーザー登録・ログインログアウト機能を実装]

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  has_many :children, dependent: :destroy
+
+  validate :children_limit
+
+  private
+
+  def children_limit
+    if self.children.size > 3
+      errors.add(:children, "は3人まで登録できます")
+    end
+  end
 end
