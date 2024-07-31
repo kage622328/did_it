2b87e916bb4ace4643cb2afe6c02925ec35eb047
kage622328 <petit23_ma@yahoo.co.jp>
2024-07-31 11:12:01 +0900

    [devise]導入 db:migrate済み

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,7 @@
 class User < ApplicationRecord
-  #authenticates_with_sorcery!
-  has_many :children, dependent: :destroy
-
-  validates :email, presence: true, uniqueness: true
-  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
-  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-
-  validate :children_limit
-
-  private
-
-  def children_limit
-    if self.children.size > 3
-      errors.add(:children, "は3人まで登録できます")
-    end
-  end
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
 end
