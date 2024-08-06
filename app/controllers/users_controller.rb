666b86250762d133ca05460c8a62ba08d12e89f2
kage622328 <petit23_ma@yahoo.co.jp>
2024-08-06 16:50:42 +0900

    [child登録機能]実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,9 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show]
   
   def show
-    @children = current_user.children 
-  end
-
-  private
-
-  def set_user
     @user = User.find(params[:id])
+    @children = current_user.children.order(created_at: :asc)
+    @child = Child.new
   end
 
 end
