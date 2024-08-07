415e6aaf99e24c48f3ad8668d09363bd28691f51
kage622328 <petit23_ma@yahoo.co.jp>
2024-08-07 15:27:31 +0900

    [お子さま登録機能]修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!  
+  before_action :authenticate_user!
+  add_flash_types :success, :danger
   
   def after_sign_in_path_for(resource)
     user_path(id: current_user.id)
