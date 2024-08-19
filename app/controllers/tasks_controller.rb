b583b127679d14ec8fb9f45e0999fc87074d863c
kage622328 <petit23_ma@yahoo.co.jp>
2024-08-19 16:03:57 +0900

    [completed_tasks/index]controller、view実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,22 @@
 class TasksController < ApplicationController
+  after_action :increment_completed_task_count, if: :status_completed?
 
-  def change_status   
+  def change_status
     @task = Task.find(params[:id])
     @task.completed! if @task.incomplete?
     
-    redirect_to list_path(@task.list_id)
+    redirect_to list_path(@task.list_id) and return
+  end
+
+  private
+
+  def status_completed?
+    @task.completed?
+  end
+
+  def increment_completed_task_count
+    completed_task = CompletedTask.find_or_initialize_by(task_body: @task.body, child_id: @task.list.child.id)
+    completed_task.completed_count += 1
+    completed_task.save
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_18_060114) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_19_011705) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,7 +33,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_18_060114) do
 
   create_table "completed_tasks", force: :cascade do |t|
     t.bigint "child_id"
-    t.integer "completed_count", default: 1, null: false
+    t.integer "completed_count", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "task_body", null: false
