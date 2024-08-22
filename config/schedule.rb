9b19c68c008539b93273f0156b0700528aae91c3
kage622328 <petit23_ma@yahoo.co.jp>
2024-08-22 13:44:38 +0900

    [rakeタスク実装]schedule.rbの実装

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,13 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+# Rails.rootを使用するために必要
+
+require File.expand_path(File.dirname(__FILE__) + '/environment')
+# cronを実行する環境変数周りの設定
+ENV.each { |k, v| env(k, v) }
+rails_env = ENV['RAILS_ENV'] || :development
+# cronを実行する環境変数をセット
+set :environment, rails_env
+# cronのログの吐き出し場所
+set :output, "#{Rails.root}/log/cron.log"
 
 # Example:
 #
@@ -18,3 +24,6 @@
 # end
 
 # Learn more: http://github.com/javan/whenever
+every 1.day, at: '0:00 am' do
+  rake 'create_list_tasks:recreate_lists_and_tasks'
+end
